58e4b8a58c1153bc528c7e7dd63312cb82d70e71
Jordan Welch <jordanhwelch@gmail.com>
2022-01-17 12:52:32 -0600

    Initialize globals

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:chore_manager_mobile/chore_manager.dart';
+import 'package:chore_manager_mobile/config/globals.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  Globals.initializeHttp();
+  Globals.initializeSecureStorage();
+
   runApp(const ChoreManager());
 }
