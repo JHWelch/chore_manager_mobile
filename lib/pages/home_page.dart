dcb0f430cb38f2ed00cdde653ea3a90f0fbf6a90
Jordan Welch <jordanhwelch@gmail.com>
2022-01-19 20:31:14 -0600

    Update list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:chore_manager_mobile/components/cm_scaffold.dart';
 import 'package:chore_manager_mobile/modules/chores/chores_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:get/instance_manager.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   final ChoresController controller = Get.put(ChoresController());
@@ -12,9 +12,11 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return CMScaffold(
       title: 'Home',
-      body: ListView.builder(
-        itemBuilder: _listItem,
-        itemCount: controller.chores().length,
+      body: Obx(
+        () => ListView.builder(
+          itemBuilder: _listItem,
+          itemCount: controller.chores().length,
+        ),
       ),
     );
   }
