c7edebc00d12ab75109d2fa6fc2099df6a20a924
Jordan Welch <jordanhwelch@gmail.com>
2022-01-28 22:05:44 -0600

    Test index endpoint

--- test/data/chore_manager_web/chores/chores_adapter_test.dart
+++ test/data/chore_manager_web/chores/chores_adapter_test.dart
@@ -1,5 +1,21 @@
+import 'package:chore_manager_mobile/data/chore_manager_web/chores/chores_adapter.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../../factories/chore_factory.dart';
+import '../../../mocks/data_mocks/chore_mocks.dart';
+import '../../../mocks/mocks.dart';
+
 void main() {
-  group('index', () {});
+  setUp(givenLoggedIn);
+  group('index', () {
+    testWidgets('can return chores', (tester) async {
+      final chore = ChoreFactory().build();
+      mockChoreIndex(chores: [chore]);
+      final adapter = ChoresAdapter();
+
+      final chores = await adapter.index();
+
+      expect(chores.first, chore);
+    });
+  });
 }
