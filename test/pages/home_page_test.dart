83e26c3e35ace1de12839796872625314ec8f80a
Jordan Welch <jordanhwelch@gmail.com>
2023-03-08 21:40:00 -0600

    better group

--- test/pages/home_page_test.dart
+++ test/pages/home_page_test.dart
@@ -23,60 +23,62 @@ void main() {
 
     tearDown(Get.reset);
 
-    testWidgets('user sees chores with due dates', (tester) async {
-      final chores = ChoreFactory().listOf(3);
-      mockChoreIndex(chores: chores);
-      await tester.pumpWidget(WidgetWrapper(HomePage()));
+    group('user sees chores with due date', () {
+      testWidgets('', (tester) async {
+        final chores = ChoreFactory().listOf(3);
+        mockChoreIndex(chores: chores);
+        await tester.pumpWidget(WidgetWrapper(HomePage()));
 
-      for (final chore in chores) {
-        expect(find.text(chore.title), findsOneWidget);
-      }
-    });
+        for (final chore in chores) {
+          expect(find.text(chore.title), findsOneWidget);
+        }
+      });
 
-    testWidgets('user sees chore due date for today', (tester) async {
-      final chore = ChoreFactory().state({
-        'nextDueDate': DateTime.now(),
-      }).build();
-      mockChoreIndex(chores: [chore]);
-      await tester.pumpWidget(WidgetWrapper(HomePage()));
+      testWidgets('for today', (tester) async {
+        final chore = ChoreFactory().state({
+          'nextDueDate': DateTime.now(),
+        }).build();
+        mockChoreIndex(chores: [chore]);
+        await tester.pumpWidget(WidgetWrapper(HomePage()));
 
-      expect(find.text('today'), findsOneWidget);
-    });
+        expect(find.text('today'), findsOneWidget);
+      });
 
-    testWidgets('user sees chore due date for tomorrow', (tester) async {
-      final chore = ChoreFactory().state({
-        'nextDueDate': DateTime.now().add(const Duration(days: 1)),
-      }).build();
-      mockChoreIndex(chores: [chore]);
-      await tester.pumpWidget(WidgetWrapper(HomePage()));
+      testWidgets('for tomorrow', (tester) async {
+        final chore = ChoreFactory().state({
+          'nextDueDate': DateTime.now().add(const Duration(days: 1)),
+        }).build();
+        mockChoreIndex(chores: [chore]);
+        await tester.pumpWidget(WidgetWrapper(HomePage()));
 
-      expect(find.text('tomorrow'), findsOneWidget);
-    });
+        expect(find.text('tomorrow'), findsOneWidget);
+      });
 
-    testWidgets('for days between +2 and +6, see day name', (tester) async {
-      final dayPlus2 = DateTime.now().add(const Duration(days: 2));
-      final dayPlus6 = DateTime.now().add(const Duration(days: 6));
-      final chore1 = ChoreFactory().state({'nextDueDate': dayPlus2}).build();
-      final chore2 = ChoreFactory().state({'nextDueDate': dayPlus6}).build();
+      testWidgets('as day name for days between +2 and +6', (tester) async {
+        final dayPlus2 = DateTime.now().add(const Duration(days: 2));
+        final dayPlus6 = DateTime.now().add(const Duration(days: 6));
+        final chore1 = ChoreFactory().state({'nextDueDate': dayPlus2}).build();
+        final chore2 = ChoreFactory().state({'nextDueDate': dayPlus6}).build();
 
-      mockChoreIndex(chores: [chore1, chore2]);
-      await tester.pumpWidget(WidgetWrapper(HomePage()));
+        mockChoreIndex(chores: [chore1, chore2]);
+        await tester.pumpWidget(WidgetWrapper(HomePage()));
 
-      final dayPlus2String = DateFormat(DateFormat.WEEKDAY).format(dayPlus2);
-      final dayPlus6String = DateFormat(DateFormat.WEEKDAY).format(dayPlus6);
-      expect(find.text(dayPlus2String), findsOneWidget);
-      expect(find.text(dayPlus6String), findsOneWidget);
-    });
+        final dayPlus2String = DateFormat(DateFormat.WEEKDAY).format(dayPlus2);
+        final dayPlus6String = DateFormat(DateFormat.WEEKDAY).format(dayPlus6);
+        expect(find.text(dayPlus2String), findsOneWidget);
+        expect(find.text(dayPlus6String), findsOneWidget);
+      });
 
-    testWidgets('for day 7 on, see date formatted', (tester) async {
-      final day = DateTime.now().add(const Duration(days: 7));
-      final chore = ChoreFactory().state({'nextDueDate': day}).build();
+      testWidgets('see date formatted for day 7 on', (tester) async {
+        final day = DateTime.now().add(const Duration(days: 7));
+        final chore = ChoreFactory().state({'nextDueDate': day}).build();
 
-      mockChoreIndex(chores: [chore]);
-      await tester.pumpWidget(WidgetWrapper(HomePage()));
+        mockChoreIndex(chores: [chore]);
+        await tester.pumpWidget(WidgetWrapper(HomePage()));
 
-      final dayString = DateFormat(DateFormat.YEAR_NUM_MONTH_DAY).format(day);
-      expect(find.text(dayString), findsOneWidget);
+        final dayString = DateFormat(DateFormat.YEAR_NUM_MONTH_DAY).format(day);
+        expect(find.text(dayString), findsOneWidget);
+      });
     });
 
     testWidgets('user does not see chores with no due dates', (tester) async {
