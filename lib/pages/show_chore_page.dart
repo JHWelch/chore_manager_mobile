572376fc8cb5488b3b706273c40b8f8501a600d0
Jordan Welch <jordanhwelch@gmail.com>
2023-03-14 21:11:13 -0500

    Add more details to show

--- lib/pages/show_chore_page.dart
+++ lib/pages/show_chore_page.dart
@@ -1,23 +1,41 @@
+import 'package:chore_manager_mobile/components/action_buttons/complete_chore_action.dart';
 import 'package:chore_manager_mobile/components/cm_scaffold.dart';
 import 'package:chore_manager_mobile/modules/chores/chore.dart';
+import 'package:chore_manager_mobile/modules/chores/chores_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ShowChorePage extends StatelessWidget {
   final Chore chore;
 
-  const ShowChorePage(this.chore, {super.key});
+  ShowChorePage(this.chore, {super.key});
+
+  final controller = Get.find<ChoresController>();
 
   @override
   Widget build(BuildContext context) {
     return CMScaffold(
-        title: chore.title,
-        body: Column(children: [
-          Text(
-            chore.friendlyDueDate,
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          Text(chore.friendlyFrequency),
-          Text(chore.description ?? ''),
-        ]));
+      actions: [
+        CompleteChoreAction(choreId: chore.id, postComplete: Get.back),
+        IconButton(icon: Icon(Icons.access_alarm), onPressed: () {}),
+      ],
+      body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                chore.title,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              Text(
+                chore.friendlyDueDate,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Text('Repeats ${chore.friendlyFrequency}'),
+              Text(chore.description ?? ''),
+            ],
+          )),
+    );
   }
 }
