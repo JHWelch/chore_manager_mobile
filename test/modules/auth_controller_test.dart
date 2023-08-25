7f69994204ed8dfb9b48b06ca7509031e44b3fe4
Jordan Welch <jordanhwelch@gmail.com>
2023-08-25 00:05:48 -0500

    Add auth service tests

--- test/modules/auth_controller_test.dart
+++ test/modules/auth_controller_test.dart
@@ -1,17 +1,41 @@
+import 'package:chore_manager_mobile/data/auth/auth_service.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+
+import '../data/chore_manager_web/users/user_adapter_mocks.dart';
+import '../mocks/firebase_mocks.dart';
+import '../mocks/http_mocks.dart';
+import '../mocks/mocks.dart';
+import '../mocks/secure_storage_mocks.dart';
 
 void main() {
-  group('user has never logged in', () {
-    // setUp(() {
-    //   mockGlobals();
-    //   mockAuthTokenStorage(null);
-    // });
+  setUp(mockGlobals);
 
-    // testWidgets('secure storage is called', (tester) async {
-    //   await tester.pumpWidget(WidgetWrapper(LoginPage()));
-    //   await AuthService().setup();
+  group('init', () {
+    group('user has never logged in', () {
+      setUp(() {
+        mockAuthTokenStorage(null);
+      });
 
-    //   verify(() => Globals.storage.read(key: authTokenKey));
-    // });
+      test('auth token is set to ""', () async {
+        final auth = await Get.putAsync(AuthService().init);
+
+        expect(auth.authToken(), '');
+      });
+    });
+
+    group('user has logged in', () {
+      setUp(() {
+        mockAuthTokenStorage(mockTokenString);
+        mockAuthUserGet();
+        mockFirebaseGetToken(null);
+      });
+
+      test('auth token is set', () async {
+        final auth = await Get.putAsync(AuthService().init);
+
+        expect(auth.authToken(), mockTokenString);
+      });
+    });
   });
 }
