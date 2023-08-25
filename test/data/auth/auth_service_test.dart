588b17eb6b959fc30f49321cde18e6cb9c534cf5
Jordan Welch <jordanhwelch@gmail.com>
2023-08-25 00:08:30 -0500

    Move test into correct folder structure

--- test/data/auth/auth_service_test.dart
+++ test/data/auth/auth_service_test.dart
@@ -2,11 +2,11 @@ import 'package:chore_manager_mobile/data/auth/auth_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 
-import '../data/chore_manager_web/users/user_adapter_mocks.dart';
-import '../mocks/firebase_mocks.dart';
-import '../mocks/http_mocks.dart';
-import '../mocks/mocks.dart';
-import '../mocks/secure_storage_mocks.dart';
+import '../../mocks/firebase_mocks.dart';
+import '../../mocks/http_mocks.dart';
+import '../../mocks/mocks.dart';
+import '../../mocks/secure_storage_mocks.dart';
+import '../chore_manager_web/users/user_adapter_mocks.dart';
 
 void main() {
   setUp(mockGlobals);
