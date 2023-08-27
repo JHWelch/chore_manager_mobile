3842d28d7dfc38cf96f7113c287a58092299857f
Jordan Welch <jordanhwelch@gmail.com>
2023-08-26 21:56:31 -0500

    Initialize crashlytics

--- lib/config/globals.dart
+++ lib/config/globals.dart
@@ -1,6 +1,8 @@
 import 'package:chore_manager_mobile/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/io_client.dart';
@@ -27,5 +29,18 @@ Future<void> _initFirebase() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  _initFirebaseCrashlytics();
+  _initFirebaseMessaging();
+}
+
+void _initFirebaseCrashlytics() {
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+}
+
+void _initFirebaseMessaging() {
   firebase = FirebaseMessaging.instance;
 }
